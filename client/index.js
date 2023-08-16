4faf546178abc007b8393847da5136a961bc2387
b21827186 <misbahbilgili@gmail.com>
2023-08-16 14:44:47 +0300

    first commit

--- client/index.js
+++ client/index.js
@@ -2,13 +2,21 @@ const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = 'http://localhost:1226';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
-
+  const merkleTree = new MerkleTree(niceList);
+  // get the root
+  const root = merkleTree.getRoot();
+  // find the proof that norman block is in the list 
+  const name = 'Norman Block';
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof,
+    name
   });
 
   console.log({ gift });
